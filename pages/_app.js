c15d31b7ec58494111f8c1d8cf5fc95b8361d340
edwardbudaza <edward.budaza@yahoo.co.za>
2023-04-27 19:02:33 +0200

    added layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import 'tailwindcss/tailwind.css';
+import React, { useEffect, useState } from 'react';
 
+import 'tailwindcss/tailwind.css';
 import '../styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
