7342d257b575aa2683a761a1c8c3de6ede6e4fd3
edwardbudaza <edward.budaza@yahoo.co.za>
2023-04-28 19:04:38 +0200

    updated the PostCard component to get data from graphcms

--- services/index.js
+++ services/index.js
@@ -4,36 +4,36 @@ const graphqlAPI = process.env.NEXT_PUBLICK_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
     const query = gql`
-    query MyQuery {
-        postsConnection {
-          edges {
-            node {
-              author {
-                bio
-                name
-                id
-                photo {
+      query MyQuery {
+          postsConnection {
+            edges {
+              node {
+                author {
+                  bio
+                  name
+                  id
+                  photo {
+                    url
+                  }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImager {
                   url
                 }
-              }
-              createdAt
-              slug
-              title
-              excerpt
-              featuredImager {
-                url
-              }
-              categories {
-                name
-                slug
+                categories {
+                  name
+                  slug
+                }
               }
             }
           }
-        }
-      }      
+        }      
     `
 
     const results = await request(graphqlAPI, query);
 
-    return results.postConnection.edges;
+    return results.postsConnection.edges;
 }
