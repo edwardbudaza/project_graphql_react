4f66f7dcde66405f0b71b296e27a5a6da8090709
edwardbudaza <edward.budaza@yahoo.co.za>
2023-04-28 15:52:06 +0200

    added .env and services folder to handle graphql requests

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   return (
     <>
         <Header />
-        {Children}
+        {children}
     </>
   )
 }
---
+++ services/index.js
@@ -0,0 +1,39 @@
+import { request, gql } from 'graphql-request';
+
+const graphqlAPI = process.env.NEXT_PUBLICK_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+    const query = gql`
+    query MyQuery {
+        postsConnection {
+          edges {
+            node {
+              author {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImager {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }      
+    `
+
+    const results = await request(graphqlAPI, query);
+
+    return results.postConnection.edges;
+}
