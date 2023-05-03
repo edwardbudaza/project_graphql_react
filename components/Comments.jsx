1e5bbfe16a2edcdba1f1887b6e26cfd46565f14a
edwardbudaza <edward.budaza@yahoo.co.za>
2023-05-03 07:09:19 +0200

    created a service for posting comments as an api in pages/api and created a form to submit comments

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react';
+import moment from 'moment';
+import parser from 'html-react-parser';
 
 const Comments = () => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    // getComments(slug)
+  },[])
+
   return (
     <div>Comments</div>
   )
